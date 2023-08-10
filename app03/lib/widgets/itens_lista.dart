25d6561341fa078cf341ebddcb4960ae6207d0f6
Alexiafern <alexiafernanda718@gmail.com>
2023-08-10 15:22:31 -0300

    Modificações

--- app03/lib/widgets/itens_lista.dart
+++ app03/lib/widgets/itens_lista.dart
@@ -1,6 +1,7 @@
 import 'package:app03/modelo/objeto_data_hora.dart';
 import 'package:flutter/material.dart';
 import 'package:app03/Pagina_lista.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 
 class tudoItemLista extends StatelessWidget {
@@ -10,20 +11,48 @@ class tudoItemLista extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(7.0),
-      child: Container(
-        decoration: BoxDecoration(color: Color.fromARGB(189, 208, 0, 250),
-        borderRadius: BorderRadius.circular(10),),
-        height: 60,       
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-          Text(DateFormat("dd/MM/yyyy - HH:mm").format(mensagem_data_hora.data_hora),
-          style: TextStyle(fontSize: 15, color: Colors.black,),),
-          Text(mensagem_data_hora.titulo,
-          style: TextStyle(fontSize: 20,),),          
-        ],),
+    return Slidable(
+      endActionPane: const ActionPane(
+        motion: ScrollMotion(),
+        children: [
+          SlidableAction(
+            flex: 2,
+            onPressed: null,
+            backgroundColor: Colors.pink,
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Deletar',
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color.fromARGB(189, 208, 0, 250),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          height: 60,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                DateFormat("dd/MM/yyyy - HH:mm")
+                    .format(mensagem_data_hora.data_hora),
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                mensagem_data_hora.titulo,
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
